57b5d502ed649f60c77b607e608bf61905aaf6b3
Rizki Taufik Anindhita <rizki.anindhita@binus.ac.id>
2024-02-23 01:12:52 +0700

    assignment w7

---
+++ assignments/week-7/answer3/generator.js
@@ -0,0 +1,13 @@
+const makeid =(length) => {
+    let result = '';
+    const characters = ['word ','cmon ','her ','love ','hey ','you ','child ','home ','house ','love ','life ','man ','woman ','boy ','girl ','wife ','husband ','son ','da ','street ', 'street ','road ','road ','avenue ','avenue ','boulevard ','boulevard ','road ','school ','college ','university ','high school ','college ','university ','high school ','high school ','high school ','robert ','laptop ','computer ','phone ','tablet ','ipad ','iphone ','android ','android ','android ','android ','android ','android ','office ','game ','adventure ','world ','city ','country ','continent '];
+    let counter = 0;
+    for(let i = 0;i <length;i++){
+      let char = characters[Math.floor(characters.length * Math.random())];
+      result += char;
+      counter += 1
+    }
+    return result;
+  }
+  
+  console.log(makeid(100));
